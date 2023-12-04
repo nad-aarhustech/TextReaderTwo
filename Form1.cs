fcaf903e1cd94685d823926a5414dc609d2831fe
Nickbigbutt <60188479+Nickbigbutt@users.noreply.github.com>
2023-12-04 14:17:57 +0100

    04/12/23
    JSON, XML reader.

--- Form1.cs
+++ Form1.cs
@@ -18,32 +18,27 @@ namespace TextReaderTwo
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
 
             if (ofd.ShowDialog() == DialogResult.OK)
-
             {
                 string filePath = ofd.FileName;
                 string fileContent = File.ReadAllText(filePath);
 
                 string erJson = IsJson(fileContent);
-                bool erXml = IsXml(fileContent);
+                string erXml = IsXml(fileContent);
 
-                if (erJson == "error")
+                if (erJson != "error")
                     label1.Text = erJson;
-                else if (erXml)
-                    label1.Text = "XML";
+                else if (erXml != "error")
+                    label1.Text = erXml;
                 else
                     label1.Text = fileContent;
-
             }
-
-
-
-
         }
+
 
         public static String IsJson(string text)
         {
@@ -56,16 +51,16 @@ namespace TextReaderTwo
 
                 foreach (var element in array)
                 {
-                    
+
                     var navn = element.GetProperty("navn").GetString();
                     var adresse = element.GetProperty("adresse").GetString();
                     var tlf_nummer = element.GetProperty("tlf_nummer").GetString();
-                    
+
                     textBuilder.AppendLine($"name: {navn}, adresse: {adresse}, tlf: {tlf_nummer}");
                 }
                 return textBuilder.ToString();
 
-           
+
             }
             catch (JsonException)
             {
@@ -73,18 +68,42 @@ namespace TextReaderTwo
             }
         }
 
-        public static bool IsXml(string text)
+        public static string IsXml(string text)
         {
             try
             {
                 var xml = new XmlDocument();
                 xml.LoadXml(text);
-                return true;
+
+                StringBuilder textBuilder = new StringBuilder();
+
+                foreach (XmlNode contact in xml.GetElementsByTagName("contact"))
+                {
+                    StringBuilder contactBuilder = new StringBuilder();
+
+                    foreach (XmlNode node in contact.ChildNodes)
+                    {
+                        contactBuilder.Append(node.Name);
+                        contactBuilder.Append(": ");
+                        contactBuilder.Append(node.InnerText);
+                        contactBuilder.Append(", ");
+                    }
+
+                    textBuilder.AppendLine(contactBuilder.ToString().TrimEnd(',', ' '));
+                }
+
+                return textBuilder.ToString();
+
             }
             catch (XmlException)
             {
-                return false;
+                return "error";
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
